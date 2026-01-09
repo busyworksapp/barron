95fa30b2b531182ca8a36cc3afa458c319799171
Developer <developer@kevro.co.za>
2026-01-09 09:53:40 +0200

    Fix path in fix.php

--- fix.php
+++ fix.php
@@ -3,7 +3,7 @@
  * ONE-CLICK FIX - Creates all missing tables
  */
 
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/config/database.php';
 
 try {
     $db = new Database();
